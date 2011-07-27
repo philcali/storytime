c5376eb4c6977c4a8a3ae079b10015279ef4b0c1
Philip Cali <philip.cali@gmail.com>
2011-07-27 00:13:46 -0500

    Support for macros

--- src/main/scala/parser.scala
+++ src/main/scala/parser.scala
@@ -9,6 +9,13 @@ import scala.io.Source.{fromFile => open}
 trait StoryDiscounter extends FencedDiscounter {
   override def newChunkParser = 
     new ChunkParser with StoryChunkParser
+
+  override def blockToXHTML: Block => xml.Node = block => block match {
+    case StoryBlock(key, children, _) => 
+      StoryMode.handlers.find(_.key == key)
+                        .getOrElse(UndefinedHandler).handle(this, children)
+    case _ => super.blockToXHTML(block)
+  }
 }
 
 trait StoryChunkParser extends FencedChunkParser {
@@ -40,18 +47,53 @@ case class StoryChunk(key: String, content: String) extends Chunk {
 }
 
 object StoryMode {
- val preprocessors = collection.mutable.ListBuffer[StoryPreprocessor]()
+  val preprocessors = collection.mutable.ListBuffer[StoryPreprocessor]()
+
+  val handlers = collection.mutable.ListBuffer[StoryHandler](new BuildHandler)
+}
+
+object UndefinedHandler extends StoryHandler {
+  val key = "undefined"
+  
+  def handle(discounter: Discounter, blocks: Seq[Block]) = 
+    discounter.toXHTML(blocks)
+}
+
+class BuildHandler extends StoryHandler {
+  val key = "build"
+
+  def handle(discounter: Discounter, blocks: Seq[Block]) = blocks.headOption match {
+    case Some(UnorderedList(items)) =>
+      <ul class = "build">
+        { discounter.toXHTML(items) }
+      </ul>
+    case Some(OrderedList(items)) =>
+      <ol class = "build">
+        { discounter.toXHTML(items) }
+      </ol>
+    case Some(_) => 
+      <div class = "build">
+        { discounter.toXHTML(blocks) }
+      </div>
+    case _ => discounter.toXHTML(blocks)
+  } 
+}
+
+trait StoryHandler extends StoryKey {
+  def handle(dicounter: Discounter, blocks: Seq[Block]): xml.Node
 }
 
 case class StoryBlock (
   val key: String,
   val blocks: Seq[Block],
   val position: Position
-) extends Block
+) extends Block with StoryKey
 
-trait StoryPreprocessor {
+trait StoryKey {
   val key: String
+}
 
+trait StoryPreprocessor extends StoryKey {
   lazy val reg = ("""\[!\s?""" + key + """\s?\]""").r
 
   def process(contents: String): String
