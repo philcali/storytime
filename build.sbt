a861f8630ecc778ef18f846b22a9db622549936e
Philip Cali <philip.cali@gmail.com>
2011-08-21 18:31:47 -0500

    App preliminary support for dynamic options

--- build.sbt
+++ build.sbt
@@ -4,6 +4,8 @@ organization := "com.github.philcali"
 
 version := "0.0.1"
 
+scalacOptions += "-unchecked"
+
 libraryDependencies ++= Seq (
   "net.databinder" %% "pamflet-knockoff" % "0.2.5"
 )
--- src/main/scala/Storytime.scala
+++ src/main/scala/Storytime.scala
@@ -1,10 +1,48 @@
 package storytime
 
 object Storytime {
+  def printHelp() {
+    println(
+    """    storytime [story commands]
+
+        List of story commands:
+
+          list-templates: displays all the installed templates
+          clear-templates: Uninstall all templates
+          template-args <template-name>: displays special template arguments
+          install <[https://github.com/|file://]template-location>
+          uninstall <template-name>
+          
+        General actions:
+
+          [options] [template-name] <markdown directory or file> [template-args]
+
+        General options:
+          
+          -h: prints this help
+          -l: list commands
+          -r: recursively"""
+    )
+  }
+
   def main (args: Array[String]) {
-    args match { 
-      case Array(markdown) => DefaultTemplate(Converter(markdown))
-      case _ => println("Provide a markdown file as input")
+    if (args.contains("-h") || args.length == 0) printHelp()
+    else if (args.contains("-l")) {
+      StoryLoader.listTemplates match {
+        case Some(templates) =>
+          templates.foreach(f => println(f.getName))
+        case None => println("No templates installed.")
+      }
+    } else {
+      val recursively = args.contains("-r")
+
+      val (options, rest) = args.partition(_.startsWith("-"))
+
+      rest match { 
+        case Array(template, markdown) => DefaultTemplate(Converter(markdown))
+        case Array(markdown) => DefaultTemplate(Converter(markdown)) 
+        case _ => println("Provide a template and a markdown file")
+      }
     }
   }
 }
