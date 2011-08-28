34f7d3fff19d9a849a232763d94679a666d70c95
Philip Cali <philip.cali@gmail.com>
2011-08-27 19:13:04 -0500

    Refactored the source generation

--- core/src/main/scala/StoryCodeGenerator.scala
+++ core/src/main/scala/StoryCodeGenerator.scala
@@ -4,47 +4,72 @@ import StoryLoader.configLocation
 import java.io.File
 import scala.io.Source.{fromFile => open}
 
-trait StoryCodeGenerator extends StoryKey {
+trait LocalGeneration extends FileGeneration with StoryFileReader {
   val fileName = "%s.story" format (key)
 
   val path = new File(configLocation, key)
 
-  val storyFile: File
-
   def changed() = {
-    if (storyFile.exists && !path.exists()) {
+    if (file.exists && !path.exists()) {
       true
-    } else if (storyFile.exists() && path.exists()) {
-      storyFile.lastModified() > path.lastModified()
+    } else if (file.exists() && path.exists()) {
+      file.lastModified() > path.lastModified()
     } else {
       false
     }
   }
+}
 
-  private def writeSource() {
+trait FileGeneration extends StoryCodeGenerator {
+  val path: File 
+
+  def write() {
     val src = srcPath
 
     if (!src.exists) {
       src.mkdirs()
     }
 
-    writeScala(src)
+    writeSource(src)
   }
 
   private def srcPath() =  {
     new File("%s/src/main/scala".format(path.getAbsolutePath))
   }
 
-  private def readStory() = {
-    val story = open(storyFile).getLines.mkString("\n")
-
-    story.replaceAll("\n\n", ",")
-  }
-
-  private def writeScala(srcPath: File) {
+  private def writeSource(srcPath: File) {
     import java.io.FileWriter
 
-    val source = 
+    val scalaFile = "%sTemplate.scala".format(key.capitalize)
+
+    val writer = new FileWriter(new File(srcPath, scalaFile))
+    writer.write(generate)
+    writer.close()
+  }
+}
+
+trait StoryFileReader extends StoryCodeGenerator {
+  val file: File
+
+  def source = readStory()
+
+  private def readStory() = {
+    open(file).getLines.mkString("\n")
+  }
+}
+
+trait StoryCodeGenerator extends StoryKey {
+  def source: String 
+
+  private def parseSource() = {
+    val lines = source.split("\n")
+
+    val transformed = lines.map("    %s".format(_)).mkString("\n")
+
+    transformed.replaceAll("\n\\s{4}\n", ",\n")
+  }
+
+  def generate() = {
     """ |package storytime
         |package %s
         |import StoryMode._
@@ -54,11 +79,7 @@ trait StoryCodeGenerator extends StoryKey {
         |     %s
         |  )
         |}""".stripMargin.format(
-        key, key.capitalize, readStory()
+        key, key.capitalize, parseSource()
       )
-    
-    val writer = new FileWriter(new File(srcPath, storyFile.getName))
-    writer.write(source)
-    writer.close()
   }
 }
