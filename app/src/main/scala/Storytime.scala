87aeea07d721d4b7633223061aafdb8f8a31157c
Philip Cali <philip.cali@gmail.com>
2011-09-14 17:32:40 -0500

    Bulk app code filled in

--- app/src/main/scala/Storytime.scala
+++ app/src/main/scala/Storytime.scala
@@ -12,6 +12,7 @@ object Storytime {
           template-args <template-name>: displays special template arguments
           install <[https://github.com/|file://]template-location>
           uninstall <template-name>
+          get <template-arg>
           
         General actions:
 
@@ -31,6 +32,8 @@ object Storytime {
 
       val (options, rest) = args.partition(_.startsWith("-"))
 
+      val (tempArgs, important) = rest.partition(_.contains("="))
+
       rest match {
         case Array(singleInput) => singleInput match {
           case "list-templates" => 
@@ -46,32 +49,72 @@ object Storytime {
             }
             println("Clearing templates...")
             delete(StoryLoader.templateLocation)
-          case _ =>
+          case _ => 
+            runStory(recursively, singleInput, None, tempArgs)
         }
         case Array(firstInput, secondInput) => firstInput match {
           case "template-args" =>
-            StoryLoader.loadTemplate(secondInput).fold(println, {
-              _.arguments.foreach { arg =>
-                println("  %s: %s".format(arg.key, arg.description))        
-              }
-            })
-          case "install" =>
-          case "uninstall" =>  
-          case _ => 
-            StoryLoader.loadTemplate(firstInput).fold(println, { template =>
-              val converter = Converter(template.mode)
-              
-              val book = converter.fromFile(secondInput)
+            StoryLoader.loadTemplate(secondInput).map {
+              template =>
 
-              template(book)
-            })
+              def formatKey(arg: StoryMetaKey[_]) {
+                val req = if (arg.required) "[REQUIRED]" else ""
+
+                println("  %1$-20s  %2$s %3$s".format(arg.key, arg.description, req)) 
+              }
+
+              println("Default Keys:")
+
+              template.defaultKeys.foreach(formatKey)
+
+              if (!template.templateKeys.isEmpty) {
+                println
+                println("%s Keys:".format(secondInput))
+
+                template.templateKeys.foreach(formatKey)
+              }
+            }
+          case "install" =>
+            // TODO: download / install
+          case "uninstall" =>
+            // TODO: delete / uninstall
+          case _ => runStory(recursively, secondInput, Some(firstInput), tempArgs)
         }
       } 
     }
   }
 
-  def runStory(recurse: Boolean, md: String, template: String, args: Array[String]) {
-    
+  def runStory(recurse: Boolean, md: String, template: Option[String], 
+               args: Array[String] = Array[String]()) {
+    import java.io.File
+    import StoryKeys.defaultTemplate
+
+    val emptyMode = StoryMode(Nil)
+
+    val configs = List[ConfigFile](
+      GlobalFile, 
+      LocalFile, 
+      MdFile(new File(md))
+    )
+
+    val mode = configs.foldLeft(emptyMode) { (in, config) =>
+      in ++ config.loadMode.getOrElse(emptyMode)
+    }
+
+    val temp = template.getOrElse(mode.getOrElse(defaultTemplate, "default"))
+
+    StoryLoader.loadTemplate(temp).map { t =>
+
+      val usedMode = t.mode ++ mode
+
+      val converter = Converter(t.mode ++ mode)
+
+      val book = converter.fromFile(configs.last.input.getAbsolutePath)
+      
+      t(book)
+    }
+
+    println("Done")
   }
 }
 
